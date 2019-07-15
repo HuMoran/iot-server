c79cb118436ec35fbd0d88373217a138cd4ea97a
Tao.Hu <htax2013@gmail.com>
2019-07-15 08:58:14 +0800

    feat: 增加定时心跳功能

--- index.js
+++ index.js
@@ -13,21 +13,40 @@ const dgram = require('dgram');
 const server = dgram.createSocket('udp4');
 const dayjs = require('dayjs');
 
+const g_devices = {
+  // 11401679791: {
+  //   id: '11401679791',
+  //   ip: 'xx.xx.xx.xx',
+  //   port: '142423',
+  //   type: 'xx',
+  //   heartbeatCount: 0,
+  // }
+};
+
+function heartbeat() {
+  // console.log(`${new Date().toLocaleString()} 服务器定时心跳处理`);
+  Object.entries(g_devices).forEach(([id, value]) => {
+    if (value.heartbeatCount > 4) return;
+    value.heartbeatCount += 1;
+    server.send('00', 0, 2, value.port, value.ip);
+  });
+}
+
 function getCheckCode(str) {
   let sum = 0;
   for (let i = 0; i < str.length - 2; i += 2) {
-    console.log(sum, parseInt(str.slice(i, i + 2), 10));
     sum += parseInt(str.slice(i, i + 2), 10);
   }
   return sum.toString(16).slice(-2).padStart(2, '0');
 }
 
-function doMsg(msg) {
+function doMsg(msg, ip, port) {
   if (msg === '74696d653f') { // 查询时间
     const time = dayjs().format('YY/MM/DD HH:mm:ss');
     const week = dayjs().day() === 0 ? 7 : dayjs().day();
     const strHex = Buffer.from(`${time} ${week}`).toString('hex');
     const code = getCheckCode(strHex);
+    // console.log(`${new Date().toLocaleString()} 终端时间查询处理请求完成`);
     return `${strHex}${code}`;
   }
   const msgType = msg[0];
@@ -35,13 +54,30 @@ function doMsg(msg) {
   switch (msgType) {
     case '0': { // 登录注册或者心跳
       if (msg[1] === '0') { // 心跳
-        // TODO: 心跳处理
+        const id = msg.slice(2);
+        // console.log(`${new Date().toLocaleString()} ${id} 心跳处理`);
+        if (g_devices[id]) {
+          g_devices[id].Online = true;
+          g_devices[id].ip = ip;
+          g_devices[id].port = port;
+          g_devices[id].heartbeatCount = 0;
+        }
         break;
       }
       // 登录
       const idLen = Number(msg[1]);
       const id = msg.slice(2, (idLen + 1) * 2);
-      // TODO 存ID，验证等后续业务
+      if (!g_devices[id]) {
+        g_devices[id] = {
+          id,
+          ip,
+          port,
+          type: '',
+          heartbeatCount: 0,
+          Online: true,
+        }
+      }
+      console.log(`${new Date().toLocaleString()} ${id} 登录处理完成`);
       return '00';
     }
     default:
@@ -56,10 +92,21 @@ server.on('error', (err) => {
 });
 
 server.on('message', (msg, rinfo) => {
-  console.log(`server got: ${msg.toString('hex')} from ${rinfo.address}:${rinfo.port}`);
+  console.log(`${new Date().toLocaleString()} server got: ${msg.toString('hex')} from ${rinfo.address}:${rinfo.port}`);
   const data = msg.toString('hex');
-  const returnMsg = doMsg(data);
+
+  if (data.length === 1
+    || data.length === 2
+    || data.length === 3
+    || data.length === 4
+    || data.length > 1024) {
+    console.error(`${new Date().toLocaleString()} server got error msg: ${msg.toString('hex')} from ${rinfo.address}:${rinfo.port}`);
+    return;
+  }
+
+  const returnMsg = doMsg(data, rinfo.address, rinfo.port);
   if (returnMsg) {
+    console.log(`${new Date().toLocaleString()} server return: ${returnMsg} to ${rinfo.address}:${rinfo.port}`);
     server.send(returnMsg, 0, returnMsg.length, rinfo.port, rinfo.address);
   }
 });
@@ -67,6 +114,19 @@ server.on('message', (msg, rinfo) => {
 server.on('listening', () => {
   const address = server.address();
   console.log(`server listening ${address.address}:${address.port}`);
+  setInterval(heartbeat, 50000);
 });
 
 server.bind(60000);
+
+// // 市电关
+// 440000000f5e02753565ad7535ff010020002800300034002f00300031002f00300031002c00310034003a00300036003a0031003300293f
+// 40ee3537303135303030374071712e636f6d2c5e02753565ad7535ff01da
+
+// // 市电开
+// 440000000f5e0275354e0a7535ff010020002800300034002f00300031002f00300031002c00310034003a00340034003a0033003800298e
+// 40ee3537303135303030374071712e636f6d2c5e0275354e0a7535ff0120
+
+// // 市电关
+// 440000000f5e02753565ad7535ff010020002800300034002f00300031002f00300031002c00310034003a00340035003a00340034002946
+// 40ee3537303135303030374071712e636f6d2c5e02753565ad7535ff01da
