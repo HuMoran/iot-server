dfa156e9d9c1c42c75024ee8aea82bbed2669725
Tao.Hu <htax2013@gmail.com>
2019-07-16 00:40:16 +0800

    fix:修改发送消息从string to Buffer

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function heartbeat() {
   Object.entries(g_devices).forEach(([id, value]) => {
     if (value.heartbeatCount > 4) return;
     value.heartbeatCount += 1;
-    server.send('00', 0, 2, value.port, value.ip);
+    server.send(Buffer.from('00', 'hex'), 0, 1, value.port, value.ip);
   });
 }
 
@@ -81,16 +81,21 @@ function doMsg(msg, ip, port) {
       return '00';
     }
     case '4': { // 上报的信息
-      const idLen = msg[1];
+      const msg = '440000000f5e0275354e0a7535ff010020002800300034002f00300031002f00300031002c00310034003a00340034003a0033003800298e';
+      const idLen = Number(msg[1]);
       const id = msg.slice(2, (idLen + 1) * 2);
+      const alarmId = msg[(idLen + 1) * 2];
+      console.log('第5路');
+      
       const data = msg.slice((idLen + 1) * 2);
       const [id2, content] = data.split('ff');
+      console.log(idLen, id, data, id2, content);
+      
     }
     default:
       break;
   }
 }
-
 
 server.on('error', (err) => {
   console.log(`server error:\n${err.stack}`);
@@ -113,7 +118,8 @@ server.on('message', (msg, rinfo) => {
   const returnMsg = doMsg(data, rinfo.address, rinfo.port);
   if (returnMsg) {
     console.log(`${new Date().toLocaleString()} server return: ${returnMsg} to ${rinfo.address}:${rinfo.port}`);
-    server.send(returnMsg, 0, returnMsg.length, rinfo.port, rinfo.address);
+    const buf = Buffer.from(returnMsg, 'hex');
+    server.send(buf, 0, buf.length, rinfo.port, rinfo.address);
   }
 });
 
@@ -125,14 +131,18 @@ server.on('listening', () => {
 
 server.bind(60000);
 
+
 // // 市电关
-// 440000000f 5e02753565ad7535 ff 010020002800300034002f00300031002f00300031002c00310034003a00300036003a0031003300293f
-// 40ee3537303135303030374071712e636f6d2c5e02753565ad7535ff01da
+// 44 0000000f 5e 02753565ad7535 ff 010020002800300034002f00300031002f00300031002c00310034003a00300036003a0031003300293f
+// 440000000f 5e 0275354e0a7535 ff 010020002800300034002f00300031002f00300031002c00310034003a00340034003a0033003800298e
+// 40 ee 35 37 30 31 35 30 30 30 37 40 71 71 2e 63 6f 6d 2c 5e 02753565ad7535 ff 01 da
 
 // // 市电开
 // 440000000f5e0275354e0a7535ff010020002800300034002f00300031002f00300031002c00310034003a00340034003a0033003800298e
 // 40ee3537303135303030374071712e636f6d2c5e0275354e0a7535ff0120
-
+// 46 712208598651 00 30003153f765e07ebf4f20611f566862a58b66 ff1a4f605bb6768453675ba467094eba8fdb5165ff0c8bf767e5770bff010020002800310038002f00300034002f00320036002c00300038003a00320036003a003200320029000d000aff08624b673a77ed4fe14f59989dff1a0035002e00305143ff094f
+// 44 0000000f 00 30003153f765e07ebf4f20611f566862a58b66 ff 1a4f605bb6768453675ba467094eba8fdb5165ff0c8bf767e5770bff010020002800310038002f00300034002f00320036002c00300038003a00320036003a003200320029000d000aff08624b673a77ed4fe14f59989dff1a0035002e00305143ff0991
+// 46 712208598651 0d 1e
 // // 市电关
 // 440000000f5e02753565ad7535ff010020002800300034002f00300031002f00300031002c00310034003a00340035003a00340034002946
 // 40ee3537303135303030374071712e636f6d2c5e02753565ad7535ff01da
